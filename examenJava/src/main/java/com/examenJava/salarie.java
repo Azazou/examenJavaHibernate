88101b1d8b52b1a2f9af10416c730723144b8f34
kelian <kelian.barry@outlook.fr>
2021-06-11 16:46:32 +0200

    création table salarie en java

---
+++ examenJava/src/main/java/com/examenJava/salarie.java
@@ -0,0 +1,88 @@
+package com.Model;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "salarie")
+public class salarie {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false, unique = true)
+	private int id;
+	
+	@Column(name = "nom", nullable = true, unique = false, length = 50)
+	private String nom;
+	
+	@Column(name = "prenom", nullable = true, unique = false, length = 50)
+	private String prenom;
+	
+	@Column(name = "salaire", nullable = true, unique = false, length = 50)
+	private int salaire;
+	
+	@Column(name = "experience", nullable = true, unique = false, length = 50)
+	private int experience;
+	
+	@OneToMany(cascade=CascadeType.ALL)
+	@JoinColumn(name="client_id")
+	private Set<Produit> produits;
+	
+	@OneToOne
+	@JoinColumn(name="compte_id")
+	private Compte compte;
+
+	public Set<Produit> getProduits() {
+		return produits;
+	}
+
+	public void setProduits(Set<Produit> produits) {
+		this.produits = produits;
+	}
+
+	public directeur() {
+		super();
+	}
+
+	public directeur(String nom, String prenom) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+		
+}
